76f443fd1d24c9f6d297f4b59248cca0b17c3099
Matteo D <101110253+Orbital01@users.noreply.github.com>
2024-03-19 14:58:51 +0100

    first commit, importing Intellij essential files

---
+++ src/main/java/it/polimi/ingsw/is24am14/HelloController.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.is24am14;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module it.polimi.ingsw.is24am14 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens it.polimi.ingsw.is24am14 to javafx.fxml;
+    exports it.polimi.ingsw.is24am14;
+}
