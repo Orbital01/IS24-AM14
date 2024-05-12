6109228acb9369d014d2a20bfd2c0aa0d2dae6b2
SteBog <stefan.bogdanovic@mail.polimi.it>
2024-05-12 21:58:37 +0200

    Almost completed the RMI infrastructure, fixed some problems and now waiting for other fixes before testing the connections and game states

--- src/main/java/it/polimi/ingsw/is24am14/server/network/ServerConnection.java
+++ src/main/java/it/polimi/ingsw/is24am14/server/network/ServerConnection.java
@@ -1,14 +1,22 @@
 package it.polimi.ingsw.is24am14.server.network;
 
 import java.rmi.Remote;
+import java.util.ArrayList;
+import java.util.List;
 
-import it.polimi.ingsw.is24am14.server.model.card.Card;
-import it.polimi.ingsw.is24am14.server.model.card.GoldCard;
-import it.polimi.ingsw.is24am14.server.model.card.PlayableCard;
+import it.polimi.ingsw.is24am14.server.model.card.*;
 import it.polimi.ingsw.is24am14.server.model.game.*;
+import it.polimi.ingsw.is24am14.server.model.player.Player;
+import it.polimi.ingsw.is24am14.server.model.player.TokenColour;
+import javafx.scene.paint.Color;
 
 public interface ServerConnection extends Remote {
     void askForMove() throws Exception;
     void flipCard(int cardIndex) throws Exception;
     String getClientNickname() throws Exception;
+    void assignColor(List<TokenColour> colors, Player player) throws Exception;
+    void chooseSecretObjective(Player player, Deck<ObjectiveCard> objectiveDeck) throws Exception;
+    void drawGoldCard() throws Exception;
+    void drawResourceCard() throws Exception;
+    void drawFromFaceUp(int faceUpIndex) throws Exception;
 }
