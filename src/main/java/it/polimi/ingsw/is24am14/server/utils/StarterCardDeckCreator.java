38bbf53cfd998582d186b68849bbbce10a5203ef
Matteo D <101110253+Orbital01@users.noreply.github.com>
2024-05-13 20:17:28 +0200

    Added base classes

---
+++ src/main/java/it/polimi/ingsw/is24am14/server/utils/StarterCardDeckCreator.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.is24am14.server.utils;
+
+import it.polimi.ingsw.is24am14.server.model.card.Deck;
+import it.polimi.ingsw.is24am14.server.model.card.StarterCard;
+
+/**
+ * This class is used to create the starter card deck for the game.
+ */
+
+public class StarterCardDeckCreator {
+
+    public Deck<StarterCard> createStarterCardDeck() {
+        // Create each one of the 6 starter cards
+
+    }
+
+}
