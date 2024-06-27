c51e42ccf749256246dbe6f6e3a305a1a2b6a6f3
Matteo D <101110253+Orbital01@users.noreply.github.com>
2024-06-27 09:22:00 +0200

    added javadoc and minor fix to main class

--- src/main/java/it/polimi/ingsw/is24am14/client/GUIViewLauncher.java
+++ src/main/java/it/polimi/ingsw/is24am14/client/GUIViewLauncher.java
@@ -5,11 +5,15 @@ import it.polimi.ingsw.is24am14.server.network.ClientInterface;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * This class is the launcher for the GUI.
+ * It starts the GUI and shows the connection screen.
+ */
 public class GUIViewLauncher extends Application {
 
-    //questa classe deve far partire la GUI
-    //stage è la finestra
-    //scene è il contenuto della finestra
+    //this class must start the GUI
+    //stage is the window
+    //scene is the content of the window
 
     private Stage primaryStage;
     private ClientInterface client; //poi deve diventare una ClientInterface
@@ -20,23 +24,38 @@ public class GUIViewLauncher extends Application {
         showConnectionScreen();
     }
 
-    public static void LaunchGUI() { //era main non so se ora funziona, modificato per JAR
+    /** this method is called for starting the GUI
+    *
+     */
+    public static void LaunchGUI() {
         launch();
     }
 
+    /**
+     * This method shows the connection screen.
+     */
     public void showConnectionScreen() {
         MenuConnectionController connectionController = new MenuConnectionController(this);
         connectionController.showScene();
     }
 
+    /**
+     * This method return the main window of the GUI.
+     */
     public Stage getStage (){
         return primaryStage;
     }
 
+    /**
+     * This method gets the client.
+     */
     public ClientInterface getClient(){
         return client;
     } //idem come sopra
 
+    /**
+     * This method sets the client.
+     */
     public void setClient(ClientInterface client){
         this.client = client;
     } //idem come sopra
